7aaaf1a37412bd468f938c8ed6d8e3024adb9571
Rakibul2tr <rakibul2tr@gmail.com>
2021-09-13 21:43:46 +0600

    first push

--- js/app.js
+++ js/app.js
@@ -10,43 +10,57 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src="${product.image}">
       </div>
-      <h3>${product.title}</h3>
+      <h4>${product.title}</h4>
       <p>Category: ${product.category}</p>
+      <h6 id="point">${product.rating.rate} Rating / ${product.rating.count} Reviwe</h6>
       <h2>Price: $ ${product.price}</h2>
+      
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button id="details-btn" onclick="ditailbtn(${product.rating.rate},${product.rating.count})" class="btn bg-primary text-white">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
+
+
   }
 };
+/**
+ * add cart hendalar and product item sonkha
+ */
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
-  updatePrice("price", price);
-
-  updateTaxAndCharge();
+  updatePrice("price" , price);
+  updateTotal ()
+  // updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
-
+/**
+ * 
+ * @param {*cart price value function}id
+ * @returns 
+ */
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
-// main price update function
+/**
+ * 
+ * @param {product item price update function} id 
+ * @param {*} value 
+ */
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
-  const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  const itemtotal = convertedOldPrice + convertPrice;
+  document.getElementById(id).innerText =itemtotal.toFixed(2);
 };
 
 // set innerText function
@@ -56,7 +70,7 @@ const setInnerText = (id, value) => {
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
-  const priceConverted = getInputValue("price");
+  const priceConverted = getInputValue( "price" );
   if (priceConverted > 200) {
     setInnerText("delivery-charge", 30);
     setInnerText("total-tax", priceConverted * 0.2);
@@ -71,10 +85,47 @@ const updateTaxAndCharge = () => {
   }
 };
 
-//grandTotal update function
+/**
+ * grandTotal update function
+ */
 const updateTotal = () => {
-  const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+   const grandTotal = getInputValue("price")+ getInputValue("delivery-charge") + getInputValue("total-tax");
+console.log(grandTotal);
+  document.getElementById("finalytotal").innerText = grandTotal.toFixed(2);
 };
+
+
+/**
+ * modal ditails
+ */
+const modal = document.getElementById('modal');
+ ditailbtn = (ratin,count) =>{
+  modal.innerHTML=`
+  <div class="modal-dialog">
+  <div class="modal-content modal-section">
+    <div class="modal-header">
+      <h4 class="modal-title text-primary">This product Purchase : ${count} parson</h4>
+      <button type="button" class="btn-close bg-white" onclick="modalclos()" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+      <p class="ratingstar">
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <i class="far fa-star"></i>
+      <span class="ratingpint"> ${ratin} </span>
+      </p>
+      <button type="button" class="btn btn-primary" onclick="modalclos()" >Cancel</button>
+      <span class="text-primary fs-7 ms-2">The Api of This site : &emsp; https://fakestoreapi.com/</span>
+    </div>
+  </div>
+  </div>
+  `
+modal.style.display='block'
+}
+/**
+ * modal ditails close
+ */
+modalclos=()=>{
+  modal.style.display='none'
+}
